639cbeda317fc556ac0d0bdedc7d31d72cd1a477
mleers <mleers4@gmail.com>
2022-11-15 11:45:36 -0800

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,18 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by mitch on 11/15/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by mitch on 11/15/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
